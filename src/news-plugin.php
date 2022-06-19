278f243af810f0eb67ad2169cbcc0e55b255080e
miguel-henrique <miguelhenrique.as10@gmail.com>
2022-06-19 00:49:24 -0300

    update

--- src/news-plugin.php
+++ src/news-plugin.php
@@ -10,14 +10,33 @@
 
 function get_news() {
     /** @var array|WP_Error $response */
-$response = wp_remote_get( 'https://newsapi.org/v2/everything?q=${brasil}&apiKey=c6595ed7516847f4a77b8fc01f2f9e6f' );
+$response = wp_remote_get( 'https://newsapi.org/v2/everything?q=${brasil}&apiKey=c6595ed7516847f4a77b8fc01f2f9e6f', array(
+    'timeout' => 20,
+    'user-agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:20.0) Gecko/20100101 Firefox/20.0'
+  ) );
  
 if ( is_array( $response ) && ! is_wp_error( $response ) ) {
     $headers = $response['headers']; // array of http header lines
     $body    = $response['body']; // use the content
 
-    echo "<pre>";
-    print_r( $body );
+
+    /* echo "<pre>";
+    print_r($body); */
+
+    $data = json_decode($body);
+    $news_data = $data->article;
+
+    if ( ! is_wp_error($news_data)){
+        echo '<ul>';
+
+        foreach($news_data as $news){
+            echo '<li>';
+             echo '<a nome: $news->title;
+            echo '</li>';
+    }
+
+    echo '</ul>'
+}
 }
 }
 add_action('wp_head', 'get_news' );
