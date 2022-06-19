2a6dbea2cffecb610e12f51080e696322b2e5900
miguel-henrique <miguelhenrique.as10@gmail.com>
2022-06-18 22:03:26 -0300

    update

--- src/news-plugin.php
+++ src/news-plugin.php
@@ -1,11 +1,23 @@
 <?php
 /**
- * Plugin Name: Meu Plugin
+ * Plugin Name: News
+ * Plugin URI: https://www.pluswebb.com/
+ * Description: Plugin de Noticias
+ * Version: 1.0
+ * Author: Miguel Henrique
+ * Author URI: http://pluswebb.com/
  */
 
-function my_content_footer( $content ) {
-    if ( is_single( )) {
-        return $content . '<p>MUITO OBRIGADO PELA SUA VISITA!</p>';
-    }
+function get_news() {
+    /** @var array|WP_Error $response */
+$response = wp_remote_get( 'https://newsapi.org/v2/everything?q=${brasil}&apiKey=c6595ed7516847f4a77b8fc01f2f9e6f' );
+ 
+if ( is_array( $response ) && ! is_wp_error( $response ) ) {
+    $headers = $response['headers']; // array of http header lines
+    $body    = $response['body']; // use the content
+
+    echo "<pre>";
+    print_r( $body );
 }
-add_filter('the_content', 'my_content_footer');
+}
+add_action('wp_head', 'get_news' );
